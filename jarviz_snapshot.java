a5e13ec1fe6ff083cb5d42b83cadbaadb5819c46
jreleaserbot <jreleaser@kordamp.org>
2022-12-11 20:29:37 +0000

    jarviz-cli early-access

--- jarviz_snapshot.java
+++ jarviz_snapshot.java
@@ -1,7 +1,8 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 11+
-//DEPS com.github.kordamp.jarviz:jarviz-cli:0.0.0-SNAPSHOT
-// Generated with JReleaser 1.4.0-SNAPSHOT at 2022-12-11T16:53:28.062513076Z
+//REPOS jitpack
+//DEPS com.github.kordamp.jarviz:jarviz-cli:main-SNAPSHOT
+// Generated with JReleaser 1.4.0-SNAPSHOT at 2022-12-11T20:29:07.496549981Z
 
 public class jarviz_snapshot {
     public static void main(String... args) throws Exception {
