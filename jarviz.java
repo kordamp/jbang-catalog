732b6825cf8a21c53c7799693e4500c41c7b4a3f
jreleaserbot <jreleaser@kordamp.org>
2023-02-06 20:30:23 +0000

    jarviz v0.2.0

--- jarviz.java
+++ jarviz.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 11+
-//DEPS org.kordamp.jarviz:jarviz-cli:0.1.0
-// Generated with JReleaser 1.3.1 at 2022-12-18T17:17:36.243322061Z
+//DEPS org.kordamp.jarviz:jarviz-cli:0.2.0
+// Generated with JReleaser 1.5.0-SNAPSHOT at 2023-02-06T20:28:32.478135496Z
 
 public class jarviz {
     public static void main(String... args) throws Exception {
