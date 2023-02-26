f482ba053c50ce948c89525ebe731f7c0e434d03
jreleaserbot <jreleaser@kordamp.org>
2023-02-26 11:17:55 +0000

    naum early-access

---
+++ naum_snapshot.java
@@ -0,0 +1,11 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 11+
+//REPOS jitpack
+//DEPS com.github.kordamp.naum:naum-cli:development-SNAPSHOT
+// Generated with JReleaser 1.5.0-SNAPSHOT at 2023-02-26T11:17:43.11671379Z
+
+public class naum_snapshot {
+    public static void main(String... args) throws Exception {
+        org.kordamp.naum.cli.Main.main(args);
+    }
+}
