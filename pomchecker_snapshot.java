a2218f064a03f71425579cc5b55139c997fb097d
jreleaserbot <jreleaser@kordamp.org>
2023-04-24 08:02:15 +0000

    pomchecker early-access

--- pomchecker_snapshot.java
+++ pomchecker_snapshot.java
@@ -2,7 +2,7 @@
 //JAVA 8+
 //REPOS jitpack
 //DEPS com.github.kordamp.pomchecker:pomchecker-cli:master-SNAPSHOT
-// Generated with JReleaser 1.5.1 at 2023-04-24T07:59:51.11872Z
+// Generated with JReleaser 1.5.1 at 2023-04-24T08:01:56.566871Z
 
 public class pomchecker_snapshot {
     public static void main(String... args) throws Exception {
