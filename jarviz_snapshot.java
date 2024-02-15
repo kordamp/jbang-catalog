319c80e9f03a621050c6352ab1e88de03ed0bd4d
jreleaserbot <jreleaser@kordamp.org>
2024-02-15 10:01:34 +0000

    jarviz early-access

--- jarviz_snapshot.java
+++ jarviz_snapshot.java
@@ -2,7 +2,7 @@
 //JAVA 11+
 //REPOS jitpack
 //DEPS com.github.kordamp.jarviz:jarviz-cli:development-SNAPSHOT
-// Generated with JReleaser 1.6.0-SNAPSHOT at 2023-03-13T10:55:25.822863574Z
+// Generated with JReleaser 1.11.0-SNAPSHOT at 2024-02-15T10:01:10.48339423Z
 
 public class jarviz_snapshot {
     public static void main(String... args) throws Exception {
