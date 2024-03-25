8cdbaa26af39dd3ca1edc72b5c141e13446c1953
jreleaserbot <jreleaser@kordamp.org>
2024-03-25 18:04:54 +0000

    pomchecker v1.11.0

--- pomchecker.java
+++ pomchecker.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8+
-//DEPS org.kordamp.maven:pomchecker-cli:1.10.0
-// Generated with JReleaser 1.9.0 at 2023-11-30T13:47:55.227517669Z
+//DEPS org.kordamp.maven:pomchecker-cli:1.11.0
+// Generated with JReleaser 1.11.0 at 2024-03-25T18:01:40.230608884Z
 
 public class pomchecker {
     public static void main(String... args) throws Exception {
